aa67a6837e9cd451b1d8e57b1bf67d18b0be5855
Rajat Gupta <7rajatgupta@gmail.com>
2019-09-25 00:19:23 +0530

    route-set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Shop from "./components/Shop";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Fortnite Store.</h1>
+      <About />
+      <Shop />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <h3>Logo here</h3>
+      <ul className="links">
+        <li>About</li>
+        <li>Shop</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
